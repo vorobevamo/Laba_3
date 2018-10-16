2605835da906c1f493c4b4a68ce7146bda584cce
Маришка <@-PC>
2018-10-16 10:03:07 +0400

    Добавьте файлы проекта.

---
+++ Лаба_3(2)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Лаба_3_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text, path;
+            Console.WriteLine("Ввести текст");
+            text = Console.ReadLine();
+            Console.WriteLine("Введите путь и имя файла для сохранения в формате C:\\File.txt:", Encoding.UTF8);
+            path = Console.ReadLine();
+            File.WriteAllText(path, text);
+            Console.WriteLine("Сохранено успешно");
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
